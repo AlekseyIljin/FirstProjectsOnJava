5c64e92f6d526b3ff4aa4ac03ec51ef142a932bf
Aleksey Iljin <master.iljin97@gmail.com>
2018-01-11 16:58:42 +0200

    Структурировал нормально все проекты на данном этапе 10.01.18

--- src/lesson_themes/drawing/Table.java
+++ src/lesson_themes/drawing/Table.java
@@ -1,27 +1,30 @@
 package lesson_themes.drawing;
 
-import java.util.Scanner;
-
 public class Table {
     public static void main(String[] args) {
-        System.out.println("Таблица для какого числа вам нужна?");
-        Scanner in = new Scanner(System.in);
-        int b = in.nextInt();
-        multiplication(b);
-        System.out.println("\nИ реверс\n");
-        reverseMultiplication(b);
+        multiplication();
+        System.out.print("\nИ реверс\n");
+        reverseMultiplication();
     }
 
-    public static void multiplication(int a) {
-        for (int i = 0; i <= 10; i++) {
-                int c = a*i;
-            System.out.println("|   " + a+"*"+i + "=" + c + "   |");
+    public static void multiplication() {
+        for (int i = 1; i <= 10; i++) {
+                for(int j = 0;j<=10;j++){
+                    int c= 0;
+                     c = i*j;
+                    System.out.print("|   "+i+"*"+j+"="+c+"   |"+"\t");
+                }
+            System.out.println("\t");
         }
     }
-    public static void reverseMultiplication(int a){
-        for (int i = 10; i >=0; i--) {
-            int c = a*i;
-            System.out.println("|   " + a+"*"+i + "=" + c + "   |");
+    public static void reverseMultiplication(){
+        for (int i = 10; i >0; i--) {
+            for(int j = 10;j>=0;j--){
+                int c= 0;
+                c = i*j;
+                System.out.print("|   "+i+"*"+j+"="+c+"   |"+"\t");
+            }
+            System.out.println("\t");
         }
     }
 }
